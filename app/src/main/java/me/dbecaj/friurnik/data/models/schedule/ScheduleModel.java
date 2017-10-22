5af07cb8cf09b63edb40605a611534783fcc4d0d
dbecaj <dominik.becaj@gmail.com>
2017-10-22 20:28:15 +0200

    Finally got the schedule for a student

--- app/src/main/java/me/dbecaj/friurnik/data/models/schedule/ScheduleModel.java
+++ app/src/main/java/me/dbecaj/friurnik/data/models/schedule/ScheduleModel.java
@@ -4,6 +4,9 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import timber.log.Timber;
 
 /**
@@ -12,17 +15,101 @@ import timber.log.Timber;
 
 public class ScheduleModel {
 
-    private SubjectModel schedule[][] = new SubjectModel[5][13];
+    public static final String MON = "MON";
+    public static final String TUE = "TUE";
+    public static final String WED = "WED";
+    public static final String THU = "THU";
+    public static final String FRI = "FRI";
+
+    // URA {7:00, 21:00} DAN {PON, PET)
+    // [URA]->(PON, TOR, SRE, CET, PET)
+    private HashMap<String, ArrayList<SubjectModel>> schedule = new HashMap<>(5);
+
+    private void initHash() {
+        schedule.put(MON, new ArrayList<SubjectModel>());
+        schedule.put(TUE, new ArrayList<SubjectModel>());
+        schedule.put(WED, new ArrayList<SubjectModel>());
+        schedule.put(THU, new ArrayList<SubjectModel>());
+        schedule.put(FRI, new ArrayList<SubjectModel>());
+    }
 
     public void parseHtml(String html){
+        initHash();
+
         Document document = Jsoup.parse(html);
         Element table = document.body().getElementsByTag("table").first();
-        for(Element ele : table.getAllElements()) {
-            Timber.d(ele.tagName());
+        int hourCount = 7;
+        for(Element hour : table.getElementsByAttributeValue("class", "timetable")) {
+           for(Element day : hour.children().next()) {
+               // This are usually the first row and are just used for padding the table in HTML
+               if(day.children().size() == 0) {
+                   continue;
+               }
+
+               Timber.d(day.className());
+
+               if(day.className().contains(ScheduleModel.MON)) {
+                   SubjectModel subject = extractSubject(day, hourCount);
+
+                   schedule.get(MON).add(subject);
+
+               }
+               else if(day.className().contains(ScheduleModel.TUE)) {
+                   SubjectModel subject = extractSubject(day, hourCount);
+
+                   schedule.get(TUE).add(subject);
+               }
+               else if(day.className().contains(ScheduleModel.WED)) {
+                   SubjectModel subject = extractSubject(day, hourCount);
+
+                   schedule.get(WED).add(subject);
+               }
+               else if(day.className().contains(ScheduleModel.THU)) {
+                   SubjectModel subject = extractSubject(day, hourCount);
+
+                   schedule.get(THU).add(subject);
+               }
+               else if(day.className().contains(ScheduleModel.FRI)) {
+                   SubjectModel subject = extractSubject(day, hourCount);
+
+                   schedule.get(FRI).add(subject);
+               }
+           }
+           hourCount++;
+        }
+
+        for(String day : schedule.keySet()) {
+            Timber.d(day + "===================>");
+            for(SubjectModel subjectModel : schedule.get(day)) {
+                Timber.d(subjectModel.getName() + ":" + subjectModel.getClassroom() +
+                        ":" + String.valueOf(subjectModel.getStartHour()) + "/" +
+                        String.valueOf(subjectModel.getEndHour()));
+            }
         }
     }
 
-    public SubjectModel[][] getSchedule() {
+    private SubjectModel extractSubject(Element day, int hourCount) {
+        int startHour = hourCount;
+        int endHour = startHour + Integer.parseInt(day.attributes().get("rowspan"));
+        String name = "";
+        String classroom = "";
+
+        // Just getting the subject info which is inside a div tag
+        Element firstDiv = day.getElementsByTag("div").first();
+        Timber.d(String.valueOf(day.getElementsByTag("div").size()));
+        for(Element subjectInfo : firstDiv.getElementsByTag("a")) {
+            if(subjectInfo.className().equals("activity")) {
+                name = subjectInfo.text();
+            }
+            else if(subjectInfo.className().equals("classroom")) {
+                classroom = subjectInfo.text();
+            }
+        }
+
+        return new SubjectModel(name, classroom, startHour, endHour);
+    }
+
+    public HashMap<String, ArrayList<SubjectModel>> getSchedule() {
         return schedule;
     }
 }
--- app/src/main/java/me/dbecaj/friurnik/data/models/schedule/SubjectModel.java
+++ app/src/main/java/me/dbecaj/friurnik/data/models/schedule/SubjectModel.java
@@ -7,13 +7,30 @@ package me.dbecaj.friurnik.data.models.schedule;
 public class SubjectModel {
 
     private String name;
+    private String classroom;
+    private int startHour;
+    private int endHour;
 
-    public SubjectModel(String name) {
+    public SubjectModel(String name, String classroom, int startHour, int endHour) {
         this.name = name;
+        this.classroom = classroom;
+        this.startHour = startHour;
+        this.endHour = endHour;
     }
 
     public String getName() {
         return name;
     }
 
+    public int getStartHour() {
+        return startHour;
+    }
+
+    public int getEndHour() {
+        return endHour;
+    }
+
+    public String getClassroom() {
+        return classroom;
+    }
 }
