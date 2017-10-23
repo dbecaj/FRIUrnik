d40dbc0bd6242a08affdf1dc8b8172321e7c5dbf
dbecaj <dominik.becaj@gmail.com>
2017-10-23 21:12:28 +0200

    Making progress on writing tests for Login, must find a way to Mock DBFlow table

--- app/src/test/java/me/dbecaj/friurnik/ui/login/LoginPresenterTest.java
+++ app/src/test/java/me/dbecaj/friurnik/ui/login/LoginPresenterTest.java
@@ -1,5 +1,8 @@
 package me.dbecaj.friurnik.ui.login;
 
+import com.raizlabs.android.dbflow.config.DatabaseDefinition;
+import com.raizlabs.android.dbflow.config.FlowManager;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,6 +10,9 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import me.dbecaj.friurnik.R;
+import me.dbecaj.friurnik.data.database.FRIUrnikDatabase;
+import me.dbecaj.friurnik.data.models.student.StudentModel;
+import me.dbecaj.friurnik.data.models.student.StudentModel_Table;
 import me.dbecaj.friurnik.data.system.ResourceProvider;
 
 import static org.mockito.Mockito.verify;
@@ -16,6 +22,7 @@ import static org.mockito.Mockito.when;
 public class LoginPresenterTest {
 
     private LoginMvp.Presenter presenter;
+
     @Mock
     private LoginMvp.View view;
 
@@ -31,4 +38,24 @@ public class LoginPresenterTest {
 
         verify(view).showStudentIdInputError(R.string.error_empty_student_id);
     }
+
+    @Test
+    public void shouldShowErrorMessageWhenStudentIdHasIncorrectDigetsLength() throws Exception {
+        when(view.getStudentId()).thenReturn("123");
+        presenter.processNextClicked();
+
+        verify(view).showStudentIdInputError(R.string.error_invalid_student_id);
+    }
+
+    @Test
+    public void shouldShowErrorMessageWhenStudentIdAlreadyExistsInStudentTable() throws Exception {
+        DatabaseDefinition database = FlowManager.getDatabase(FRIUrnikDatabase.class);
+        StudentModel_Table table = new StudentModel_Table(database);
+        table.insert(new StudentModel(63170050, true));
+        when(view.getStudentId()).thenReturn("63170050");
+        presenter.processNextClicked();
+
+        verify(view).showStudentIdInputError(R.string.error_student_already_in_database);
+    }
+
 }
